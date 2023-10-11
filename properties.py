90ad2e0ebc5bb5ff4bf4ca57911fb2ce739c299c
Adam Elamin <116977132+aaelamin@users.noreply.github.com>
2023-10-11 14:44:05 -0400

    added container function and odified main function

--- properties.py
+++ properties.py
@@ -4,21 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
-from time import sleep
 import pandas as pd
 
-def load_page(driver, city, type, page):
+def load_page( city, type, page):
+    driver = webdriver.Chrome()
     url = f"https://condos.ca/{city}/condos-for-{type}?page={page}"
     driver.get(url)
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'lxml')
     return soup
 
+def containers(soup):
+    div = soup.find_all('div', {'class':"styles___PreviewContent-sc-54qk44-3 kGcPgm"})
+    return div
 
 def main():
-    driver = webdriver.Chrome()
-    city = "Toronto"
-    type = "Rent"
-    page = 3
-    soup = load_page(driver, city, type, page)
+    city = input("Enter the city:\n")
+    type = input("Condos for sale or rent:\n")
+    soup = load_page(city, type, 1)
+    listings = containers(soup)
+
 main()
