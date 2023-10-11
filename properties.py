e2ab018e6c264abed01078c6675a27ffa682a061
Adam Elamin <116977132+aaelamin@users.noreply.github.com>
2023-10-10 21:39:44 -0400

    created load_page to get the source page of the website

--- properties.py
+++ properties.py
@@ -4,5 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
-
+from time import sleep
 import pandas as pd
+
+def load_page(driver, city, type, page):
+    url = f"https://condos.ca/{city}/condos-for-{type}?page={page}"
+    driver.get(url)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    return soup
+
+
+def main():
+    driver = webdriver.Chrome()
+    city = "Toronto"
+    type = "Rent"
+    page = 3
+    soup = load_page(driver, city, type, page)
+    print(soup.prettify())
+main()
